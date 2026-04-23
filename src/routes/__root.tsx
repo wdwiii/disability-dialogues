f5a40ec4089e1e7640902b72c20fefdf8453452f
wdwiii <wdwhitfield3@gmail.com>
2026-04-23 02:18:05 -0400

    Meta Title update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,7 +21,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Disability Dialogs: Beyond Accommodations',
+        title: 'Disability Dialogues: Beyond Accommodations',
       },
     ],
     links: [
