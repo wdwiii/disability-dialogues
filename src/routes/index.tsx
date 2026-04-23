d42e5973bfaf2dd66e7f0e3dc96a5da13dc48e56
wdwiii <wdwhitfield3@gmail.com>
2026-04-23 00:26:20 -0400

    Add responsive sizing and display font

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,9 +34,11 @@ const Page = React.forwardRef<
 
 function App() {
   return (
-    <main className=" flex flex-col items-center justify-center gap-5  h-screen overflow-hidden bg-[url(/public/01_beyond_accommodations/front.jpg),url(/public/01_beyond_accommodations/back.jpg)] bg-contain relative px-5">
-      <div className="bg-linear-to-br from-black/95 via-purple-950/95 to-black/95 h-full w-full absolute left-0 top-0"></div>
-      {/* <h1>Disability Dialogues: Beyond Accommodations</h1> */}
+    <main className=" flex flex-col items-center justify-center gap-5  h-svh overflow-hidden bg-[url(/public/01_beyond_accommodations/front.jpg),url(/public/01_beyond_accommodations/back.jpg)] bg-contain relative px-5">
+      <div className="bg-linear-to-br from-black/95 via-purple-950/95 to-black/95 h-full w-full absolute left-0 top-0 "></div>
+      <h1 className="text-purple-50 text-3xl lg:text-6xl text-center text-balance font-medium z-10 font-display3">
+        Disability Dialogues: Beyond Accommodations
+      </h1>
       <div className="max-w-5xl w-full">
         <HTMLFlipBook
           width={550}
